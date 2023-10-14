0617bac971af1655657dedb8e9020ad5de86706e
Rachel Allen <rachel.allen1@students.snow.edu>
2023-10-14 15:00:12 -0600

    pausing at section 3 video 3, "implementing a DB context"

---
+++ WiredBrainCoffeeBlazorServer/Data/Models/Department.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WiredBrainCoffeeBlazorServer.Data.Models;
+
+public class Department
+{
+    public int ID { get; set; }
+    [Required]
+    [StringLength(50)]
+    public string? Name { get; set; }
+
+    //to let us access employees in each department
+    public List<Employee> Employees { get; set; } = new();
+}
---
+++ WiredBrainCoffeeBlazorServer/Data/Models/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WiredBrainCoffeeBlazorServer.Data.Models;
+
+public class Employee
+{
+    public int Id { get; set; }
+
+    [Required]
+    [StringLength(50)]
+    public string? FirstName { get; set; }
+    [Required]
+    [StringLength(50)]
+    public string? LastName { get; set; }
+
+    public bool IsDeveloper { get; set; }
+
+
+    //So we can track which department an employee belongs to
+    [Required]
+    public int? DepartmentID { get; set; }
+    
+    public Department? Department { get; set; }
+}
